60bb94a74470259fcf5434bbd302b6d0887b5742
Torsten Fink <torsten.fink@akquinet.de>
2016-04-07 20:44:06 +0200

    added wildfly app server

--- jpapitfalls-project/jpapitfalls-docker/scripts/startContainers.sh
+++ jpapitfalls-project/jpapitfalls-docker/scripts/startContainers.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
 docker run --name jpapitfalls-rdbms --detach --publish :5432:5432 jpapitfalls-postgres
+
+docker run --interactive --tty\
+  --publish :8080:8080 --publish :9090:9090\
+  --name jpapitfalls-appserver\
+  jpapitfalls-appserver\
+  /opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
