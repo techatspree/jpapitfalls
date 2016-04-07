b28c4c8e7f3a2516e5bc060cab06aa7864eb6822
Torsten Fink <torsten.fink@akquinet.de>
2016-04-07 21:00:48 +0200

    added deployment

--- jpapitfalls-project/jpapitfalls-docker/scripts/startContainers.sh
+++ jpapitfalls-project/jpapitfalls-docker/scripts/startContainers.sh
@@ -4,6 +4,6 @@ docker run --name jpapitfalls-rdbms --detach --publish :5432:5432 jpapitfalls-po
 
 docker run --interactive --tty\
   --publish :8080:8080 --publish :9090:9090\
-  --name jpapitfalls-appserver\
+  --name jpapitfalls-app\
   jpapitfalls-appserver\
   /opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
